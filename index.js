359df413ff4b99779c1272845826a5a2ce05e749
Abhishek-HACK <abhishekswami332025@gmail.com>
2026-03-23 13:37:29 +0530

    API

--- index.js
+++ index.js
@@ -2,12 +2,20 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth")
 
 mongoose.connect(process.env.MONGO_URL)
 .then(() =>console.log("db connected successfull!"))
 .catch((err) => console.log("error connnecting to db" , err)
 );
 
-app.listen(5000, () => {
+app.use(express.json());
+ 
+app.use("/api/auth" , authRoute);
+app.use("/api/users" , userRoute); 
+
+
+app.listen(process.env.PORT, () => {
     console.log("backend server is running");
 });
